e8315850dfa686928d836408cd4136618a1fa29c
chenghsj <43364792+chenghsj@users.noreply.github.com>
2020-09-25 02:05:24 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvIDRxE5VmrG0LmbgTrq4OXSPHd6pKSPQ",
-  authDomain: "messenger-clone-1b860.firebaseapp.com",
-  databaseURL: "https://messenger-clone-1b860.firebaseio.com",
-  projectId: "messenger-clone-1b860",
-  storageBucket: "messenger-clone-1b860.appspot.com",
-  messagingSenderId: "92868085165",
-  appId: "1:92868085165:web:6444d9daeaf9f3f3fb4d24",
-  measurementId: "G-X74VY0QQWM",
+ 
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
